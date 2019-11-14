73292b83b76620f177164891b28a2cc3af92ab01
s-lijer <s-lijer@53S500038105.it.bsd405.org>
2019-11-14 08:33:53 -0800

    Turning in Checkpoint 2

--- src/main/java/fracCalc/FracCalc.java
+++ src/main/java/fracCalc/FracCalc.java
@@ -1,32 +1,84 @@
-/**
- * @author Mr. Rasmussen
- */
-
 package fracCalc;
+import java.util.*;
 
 public class FracCalc {
-
-    public static void main(String[] args)
-    {
-        // TODO: Read the input from the user and call produceAnswer with an equation
-
+    public static void main(String[] args) {
+    	Scanner userInput = new Scanner(System.in);
+    	System.out.print("Welcome to Fraction Calculator! ");
+    	System.out.println("Enter your operations below.");
+    	boolean check = true;
+    	while (check) {
+        	String value = userInput.nextLine();
+        	check = checkForQuit(value);
+        	
+        	if (check) {
+        		String output = produceAnswer(value);
+            	System.out.println(output);
+        	}
+    	}
+    	System.out.println("Exiting Fraction Calculator.");
+    	userInput.close();
     }
 
-    // ** IMPORTANT ** DO NOT DELETE THIS FUNCTION.  This function will be used to test your code
-    // This function takes a String 'input' and produces the result
-    //
-    // input is a fraction string that needs to be evaluated.  For your program, this will be the user input.
-    //      e.g. input ==> "1/2 + 3/4"
-    //
-    // The function should return the result of the fraction after it has been calculated
-    //      e.g. return ==> "1_1/4"
-    public static String produceAnswer(String input)
-    {
-        // TODO: Implement this function to produce the solution to the input
-
-        return "";
+    public static String produceAnswer(String input) {
+    	String operand1 = input.substring(0,input.indexOf(' '));
+    	input = input.substring(input.indexOf(' ') + 1);
+    	String operator = input.substring(0, 1);
+    	input = input.substring(2);
+    	String operand2 = input;
+    	
+    	int whole1 = parseWhole(operand1);
+    	int numer1 = parseNumer(operand1);
+    	int denom1 = parseDenom(operand1);
+    	
+    	int whole2 = parseWhole(operand2);
+    	int numer2 = parseNumer(operand2);
+    	int denom2 = parseDenom(operand2);
+    	String components2 = ("whole:" + whole2 + " numerator:" + numer2 +
+    			" denominator:" + denom2);
+        return components2;
     }
-
-    // TODO: Fill in the space below with any helper methods that you think you will need
-
+    
+    public static boolean checkForQuit(String input) {
+    	String check = input.toLowerCase();
+    	if (check.equals("quit")) {
+    			return false;
+    	} else {
+    		return true;
+    	}
+    }
+    
+    public static int parseWhole(String operand) {
+    	if (operand.indexOf('_') != -1) {
+    		operand = operand.substring(0, operand.indexOf('_'));
+    		int whole = Integer.parseInt(operand);
+    		return whole;
+    	} else if (operand.indexOf('/') != -1) {
+    		return 0;
+    	} else {
+    		int whole = Integer.parseInt(operand);
+    		return whole;
+    	}
+    }
+    
+    public static int parseNumer(String operand) {
+        operand = operand.substring(operand.indexOf('_') + 1);
+    	if (operand.indexOf('/') != -1) {
+    		operand = operand.substring(0, operand.indexOf('/'));
+        	int numer = Integer.parseInt(operand);
+        	return numer;
+    	} else {
+    		return 0;
+    	}
+    }
+    
+    public static int parseDenom(String operand) {
+    	if (operand.indexOf('/') != -1) {
+    		operand = operand.substring(operand.indexOf('/') + 1);
+        	int denom = Integer.parseInt(operand);
+        	return denom;
+    	} else {
+    		return 1;
+    	}
+    }
 }
